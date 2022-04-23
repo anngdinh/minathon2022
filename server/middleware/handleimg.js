825be039c119fe172b410c07ac0443727f9b68a3
hoangkui <hoangkui2001@gmail.com>
2022-04-23 19:11:32 +0700

    update serve

--- server/middleware/handleimg.js
+++ server/middleware/handleimg.js
@@ -10,7 +10,8 @@ const fileFilter = (req, file, cb) => {
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/");
+    console.log("vcc");
+    cb(null, "./public");
   },
   filename: function (req, file, cb) {
     console.log("vcc");
