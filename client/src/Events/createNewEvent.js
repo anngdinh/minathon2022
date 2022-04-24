f903a94a6a17d8a3c35e2b0483963de394cfc45b
dinhan2411 <an.ngdinh@gmail.com>
2022-04-24 13:42:59 +0700

    event

--- client/src/Events/createNewEvent.js
+++ client/src/Events/createNewEvent.js
@@ -79,7 +79,7 @@ export default function NewEvent({ loading, setLoading }) {
           <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"></path>
           <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"></path>
         </svg>
-        <span class="ms-1">Tao event moi</span>
+        <span class="ms-1">Create new event</span>
       </button>
       {/* <!-- The Modal --> */}
       <div class="modal" id="myModal">
@@ -87,7 +87,7 @@ export default function NewEvent({ loading, setLoading }) {
           <div class="modal-content">
             {/* <!-- Modal Header --> */}
             <div class="modal-header">
-              <h4 class="modal-title">Thong tin su kien</h4>
+              <h4 class="modal-title">Information</h4>
               <button
                 type="button"
                 class="btn-close"
@@ -112,7 +112,7 @@ export default function NewEvent({ loading, setLoading }) {
                   required
                 />
                 <Form.Control.Feedback type="invalid">
-                  Phần này không được để trống
+                  Invalid
                 </Form.Control.Feedback>
               </Form.Group>
               <Form.Group
@@ -122,14 +122,14 @@ export default function NewEvent({ loading, setLoading }) {
                 <Form.Label>Address</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="Tên địa điểm tổ chức"
+                  placeholder="123 Street ..."
                   name="address"
                   value={address}
                   onChange={onChangeField}
                   required
                 />
                 <Form.Control.Feedback type="invalid">
-                  Phần này không được để trống
+                  Invalid
                 </Form.Control.Feedback>
               </Form.Group>
               <div class="row mt-2">
