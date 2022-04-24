0af1a5fb8fd11433d4111af6df4e4d51464786df
hoangkui <hoangkui2001@gmail.com>
2022-04-24 08:20:54 +0700

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home.js';
-import RenderEvent from './Events/RenderEvent'
-import Header from './Header'
-import Footer from './Footer'
-import RenderFruit from './Events/RenderFruit';
-import { Label } from '@mui/icons-material';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home.js";
+import RenderEvent from "./Events/RenderEvent";
+import Header from "./Header";
+import Footer from "./Footer";
+import RenderFruit from "./Events/RenderFruit";
+import { Label } from "@mui/icons-material";
 function App() {
   return (
     <>
-    {/* <Header/> */}
-    <RenderFruit/>
-    {/* <RenderEvent/> */}
-    {/* <Footer/> */}
+      {/* <Header/> */}
+      <RenderFruit />
+      {/* <RenderEvent/> */}
+      {/* <Footer/> */}
     </>
   );
 }
